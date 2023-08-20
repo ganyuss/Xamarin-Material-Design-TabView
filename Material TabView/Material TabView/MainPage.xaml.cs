8a25829cd831e03b01631bf5006f660f58cb3647
ganyuss <51406965+ganyuss@users.noreply.github.com>
2023-08-20 17:44:02 +0200

    Empty project

---
+++ Material TabView/Material TabView/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Material_TabView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
